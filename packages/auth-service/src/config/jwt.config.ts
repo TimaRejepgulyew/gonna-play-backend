5bc00b3cc8819dffa776f6a7b866cddf0083f7ad
Timur Rejepguliev <t.rejepguliev@telemedicine.ge>
2025-05-09 15:07:28 +0400

    remove all things

--- packages/auth-service/src/config/jwt.config.ts
+++
@@ -1,7 +0,0 @@
-// src/config/jwt.config.ts
-import { jwtConfig } from '.';
-
-export const JWT_SECRET = jwtConfig.secret;
-export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET || 'your_refresh_token_secret';
-export const ACCESS_TOKEN_EXPIRY = jwtConfig.expiresIn;
-export const REFRESH_TOKEN_EXPIRY = '7d'; // Example: 7 days 
--- packages/auth-service/src/types/module-alias.d.ts
+++
@@ -1,4 +0,0 @@
-declare module 'module-alias' {
-  export function addAliases(aliases: Record<string, string>): void;
-  export function addPath(path: string): void;
-} 
